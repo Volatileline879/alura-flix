aa7f087c4beeeb68c4835d0b16d1699c1d6b93c9
Volatileline879 <volatileline879@gmail.com>
2022-09-29 16:32:11 -0300

    create readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# Alura Flix
+
+![previwe](./.github/preview.jpg)
+
+Projeto construido no evento Imersão dev da Alura
+
+[Clique aqui para acessar](https://volatileline879.github.io/alura-flix/)
+
+## Tecnologias
+
+- HTML
+- CSS
+- JS
+- Git e Github
+
+## Contatos
+
+Discord: volatileline879#7753  
+Gmail: volatileline879@gmail.com
--- src/js/index.js
+++ src/js/index.js
@@ -40,7 +40,7 @@ var listaLinksFilmes = [
 	//Harry Potter 1
 	"https://play.hbomax.com/page/urn:hbo:page:GYjt1ywlfs3jDZAEAAAAM:type:feature?source=googleHBOMAX&action=open",
 	//Senhor dos Aneis
-	"https://www.primevideo.com/detail/amzn1.dv.gti.f856462e-f3f0-47d6-99a7-8e7900ffb935?ref_=dvm_pds_tit_br_dc_s_g_mkw_sdXAajrGT-dc_pcrid_618796846552&mrntrk=slid__pgrid_139847815197_pgeo_1001733_x__ptid_kwd-300625556060&gclid=CjwKCAjw4JWZBhApEiwAtJUN0Hl5QXTKUDNu_-ZkkImmL28gAdA96ySk1QjMlzMRPtqo_Boe7ORUlhoClZ4QAvD_BwE"
+	"https://play.hbomax.com/page/urn:hbo:page:GXdu2ZAglVJuAuwEAADbA:type:feature?source=googleHBOMAX&action=play"
 ];
 //End of fake data base
 
